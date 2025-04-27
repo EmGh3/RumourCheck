99e5e669beaacf7095dca8e17b999fb65c053562
yasmine-sassi <yasmine.sassi@insat.ucar.tn>
2025-04-27 14:50:13 +0100

    static dashboard

---
+++ RumourCheck_front/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RumourCheck_front.Controllers
+
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RumourCheck_front/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RumourCheck_front.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public int TotalChecks { get; set; }
+        public int TrueNews { get; set; }
+        public int FakeNews { get; set; }
+        //public List<Analysis> RecentChecks { get; set; }
+    }
+}
